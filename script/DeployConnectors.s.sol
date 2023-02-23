768a87bc19085a2adea08da0feb1f29a62bb9129
Mubaris NK <hello@mubaris.com>
2023-02-23 13:11:14 +0400

    🍓 Synthetix Spot & 1inch connectors

--- script/DeployConnectors.s.sol
+++ script/DeployConnectors.s.sol
@@ -5,7 +5,8 @@ import {Script} from "forge-std/Script.sol";
 import {console2} from "forge-std/console2.sol";
 
 import {BasicConnector} from "../src/connectors/Basic.sol";
-import {SynthetixPerpConnector} from "../src/connectors/SynthetixPerp.sol";
+import {SynthetixSpotConnector} from "../src/connectors/SynthetixSpot.sol";
+import {OneInchConnector} from "../src/connectors/OneInch.sol";
 
 interface IConnectors {
     function addConnectors(string[] calldata _connectorNames, address[] calldata _connectors) external;
@@ -19,15 +20,18 @@ contract DeployConnectors is Script {
 
         IConnectors connectors = IConnectors(0x436C89f77F6B6fbFE14d97cd9244e385FaE94FeA);
 
-        SynthetixPerpConnector synthetixPerp = new SynthetixPerpConnector();
+        SynthetixSpotConnector synthetixSpot = new SynthetixSpotConnector();
+        OneInchConnector oneInch = new OneInchConnector();
 
-        string[] memory names = new string[](1);
-        names[0] = synthetixPerp.name();
+        string[] memory names = new string[](2);
+        names[0] = synthetixSpot.name();
+        names[1] = oneInch.name();
 
-        address[] memory addrs = new address[](1);
-        addrs[0] = address(synthetixPerp);
+        address[] memory addrs = new address[](2);
+        addrs[0] = address(synthetixSpot);
+        addrs[1] = address(oneInch);
 
-        connectors.updateConnectors(names, addrs);
+        connectors.addConnectors(names, addrs);
 
         vm.stopBroadcast();
     }
