686240426922b69a37247b64ffed010962241662
Jatin Garg <jatin.garg.cse19@itbhu.ac.in>
2023-03-01 20:12:02 +0530

    initiate deposit/withdraw
    Signed-off-by: Jatin Garg <jatin.garg.cse19@itbhu.ac.in>

--- src/connectors/PowerPerp.sol
+++ src/connectors/PowerPerp.sol
@@ -23,6 +23,8 @@ struct TradeParams {
 }
 
 interface ILiquidityPool {
+    function queueDeposit(uint256 amount, address user) external;
+    function queueWithdraw(uint256 amount, address user) external;
     function deposit(uint256 amount, address user) external;
     function withdraw(uint256 tokens, address user) external;
 }
@@ -36,14 +38,56 @@ interface IExchange {
 contract PowerPerpConnector is BaseConnector {
     using SafeTransferLib for ERC20;
 
-    string public constant name = "Power-Perp";
+    string public constant name = "Power-Perp-v1";
 
     ERC20 public constant susd = ERC20(0x8c6f28f2F1A3C87F0f938b96d27520d9751ec8d9);
-    ERC20 public constant liquidityToken = ERC20(0x71055Ad10c8B0D5C8d30295CfcE32bE7aA1f1133);
-    ERC20 public constant powerPerp = ERC20(0xF80EeBec5A7BeaBE094fd043d55B28D908c12375);
-    ERC20 public constant shortToken = ERC20(0x7B15b1EbE6D51e241375FF287476D4379889DDb6);
-    address liquidityPool = 0xFEe7e4015e12C6450BEb04784fEf916a31CD79CF;
-    address exchange = 0xbd087DfDcf7739B6A6dd8167239273fB3cDcBf92;
+    ERC20 public immutable liquidityToken;
+    ERC20 public immutable powerPerp;
+    ERC20 public immutable shortToken;
+
+    address immutable liquidityPool;
+    address immutable exchange;
+
+    constructor(
+        address _liquidityToken,
+        address _powerPerp,
+        address _shortToken,
+        address _liquidityPool,
+        address _exchange
+    ) {
+        liquidityPool = _liquidityPool;
+        exchange = _exchange;
+        liquidityToken = ERC20(_liquidityToken);
+        powerPerp = ERC20(_powerPerp);
+        shortToken = ERC20(_shortToken);
+    }
+
+    function initiateDeposit(uint256 amt, uint256 getId, uint256 setId)
+        public
+        payable
+        returns (string memory _eventName, bytes memory _eventParam)
+    {
+        uint256 _amt = getUint(getId, amt);
+        _amt = _amt == type(uint256).max ? susd.balanceOf(address(this)) : _amt;
+        susd.safeApprove(liquidityPool, _amt);
+        ILiquidityPool(liquidityPool).queueDeposit(_amt, address(this));
+        setUint(setId, _amt);
+        _eventName = "LogInitiateDeposit(uint256,uint256,uint256)";
+        _eventParam = abi.encode(_amt, getId, setId);
+    }
+
+    function initiateWithdraw(uint256 amt, uint256 getId, uint256 setId)
+        public
+        payable
+        returns (string memory _eventName, bytes memory _eventParam)
+    {
+        uint256 _amt = getUint(getId, amt);
+        _amt = _amt == type(uint256).max ? liquidityToken.balanceOf(address(this)) : _amt;
+        ILiquidityPool(liquidityPool).queueWithdraw(_amt, address(this));
+        setUint(setId, _amt);
+        _eventName = "LogInitiateWithdraw(uint256,uint256,uint256)";
+        _eventParam = abi.encode(_amt, getId, setId);
+    }
 
     function deposit(uint256 amt, uint256 getId, uint256 setId)
         public
@@ -99,6 +143,8 @@ contract PowerPerpConnector is BaseConnector {
         _eventParam = abi.encode(tradeParams, getId, setId);
     }
 
+    event LogInitiateDeposit(uint256 amt, uint256 getId, uint256 setId);
+    event LogInitiateWithdraw(uint256 amt, uint256 getId, uint256 setId);
     event LogDeposit(uint256 amt, uint256 getId, uint256 setId);
     event LogWithdraw(uint256 amt, uint256 getId, uint256 setId);
     event LogOpenTrade(TradeParams tradeParams, uint256 getId, uint256 setId);
