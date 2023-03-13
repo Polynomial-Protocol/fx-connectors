2e2ef0bf188b5e31f37b5413e7d807c0be0529e1
Mubaris NK <hello@mubaris.com>
2023-03-13 10:29:54 +0400

    🧋 deploy limit orders

--- script/DeployAutomation.s.sol
+++ script/DeployAutomation.s.sol
@@ -4,14 +4,14 @@ pragma solidity ^0.8.9;
 import {Script} from "forge-std/Script.sol";
 import {console2} from "forge-std/console2.sol";
 
-import {SynthetixBasisTrading} from "../src/automations/SynthetixBasisTrading.sol";
+import {SynthetixLimitOrders} from "../src/automations/SynthetixLimitOrders.sol";
 
 contract DeployAutomation is Script {
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
 
-        new SynthetixBasisTrading();
+        new SynthetixLimitOrders();
 
         vm.stopBroadcast();
     }
