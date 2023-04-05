15e4ea57569370293c37728bfb15612737b316e9
Mubaris NK <hello@mubaris.com>
2023-04-05 18:27:13 +0400

    c

--- src/utils/TestContract.sol
+++ src/utils/TestContract.sol
@@ -5,8 +5,8 @@ import {Initializable} from "../proxy/utils/Initializable.sol";
 
 contract TestEventer is Initializable {
     function emitIt(uint256 value) external {
-        emit TheEvent(msg.sender, value, bytes32(value));
+        emit TheEvent(msg.sender, value, bytes32(value), value % 3);
     }
 
-    event TheEvent(address indexed user, uint256 value, bytes32 data);
+    event TheEvent(address indexed user, uint256 value, bytes32 data, uint256 extraData);
 }
