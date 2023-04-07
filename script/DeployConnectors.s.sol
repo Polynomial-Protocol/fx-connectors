34938c98e0073d4420fc36cf6e558f02d0c9b9e9
Mubaris NK <hello@mubaris.com>
2023-04-07 11:10:45 +0400

    🥗 deploy basis trading connector

--- script/DeployConnectors.s.sol
+++ script/DeployConnectors.s.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.9;
 import {Script} from "forge-std/Script.sol";
 import {console2} from "forge-std/console2.sol";
 
-import {SynthetixPerpConnector} from "../src/connectors/SynthetixPerp.sol";
+import {SynthetixBasisTradingConnector, IBasisTrading} from "../src/connectors/SynthBasisTrading.sol";
 
 interface IConnectors {
     function addConnectors(string[] calldata _connectorNames, address[] calldata _connectors) external;
@@ -18,13 +18,14 @@ contract DeployConnectors is Script {
 
         IConnectors connectors = IConnectors(0x436C89f77F6B6fbFE14d97cd9244e385FaE94FeA);
 
-        SynthetixPerpConnector perp = new SynthetixPerpConnector();
+        SynthetixBasisTradingConnector basis =
+            new SynthetixBasisTradingConnector(IBasisTrading(0x3349de7822aa05f857e92d167B264809419DC620));
 
         string[] memory names = new string[](1);
-        names[0] = perp.name();
+        names[0] = basis.name();
 
         address[] memory addrs = new address[](1);
-        addrs[0] = address(perp);
+        addrs[0] = address(basis);
 
         connectors.addConnectors(names, addrs);
 
