c73f19ebe478724ec46d17ad9f379c578eb58e08
Mubaris NK <hello@mubaris.com>
2023-04-11 12:27:30 +0400

    🎃 Add pyth price check

--- src/automations/SynthetixAdvancedOrders.sol
+++ src/automations/SynthetixAdvancedOrders.sol
@@ -7,6 +7,8 @@ import {Initializable} from "../proxy/utils/Initializable.sol";
 import {AuthUpgradable, Authority} from "../libraries/AuthUpgradable.sol";
 import {ReentrancyGuardUpgradable} from "../libraries/ReentracyUpgradable.sol";
 
+import {IPyth} from "../interfaces/IPyth.sol";
+
 interface IList {
     function accountID(address) external view returns (uint64);
 }
@@ -83,6 +85,9 @@ contract SynthetixLimitOrders is Initializable, AuthUpgradable, ReentrancyGuardU
     /// State Variables
     /// -----------------------------------------------------------------------
 
+    /// @notice Pyth Oracle
+    IPyth public pyth;
+
     /// @notice Address of the fee receipient
     address public feeReceipient;
 
@@ -95,10 +100,19 @@ contract SynthetixLimitOrders is Initializable, AuthUpgradable, ReentrancyGuardU
     /// @notice Next full order id
     uint256 public nextFullOrderId;
 
-    /// @notice Order id to order info mapping
-    mapping(uint256 => FullOrder) public fullOrders;
+    /// @notice Time Cutoff for Pyth Price
+    uint256 public pythPriceTimeCutoff;
 
-    function initialize(address _owner, address _feeReceipient, uint256 _flatFee, uint256 _percentageFee)
+    /// @notice Price Delta Cutoff for Pyth Price
+    uint256 public pythPriceDeltaCutoff;
+
+    /// @notice Order id to order info mapping
+    mapping(uint256 => FullOrder) private fullOrders;
+
+    /// @notice Pyth Oracle IDs to read for each market
+    mapping(address => bytes32) public pythIds;
+
+    function initialize(IPyth _pyth, address _owner, address _feeReceipient, uint256 _flatFee, uint256 _percentageFee)
         public
         initializer
     {
@@ -107,6 +121,8 @@ contract SynthetixLimitOrders is Initializable, AuthUpgradable, ReentrancyGuardU
 
         emit UpdateFeeReceipient(feeReceipient, _feeReceipient);
         emit UpdateFees(flatFee, _flatFee, percentageFee, _percentageFee);
+
+        pyth = _pyth;
 
         feeReceipient = _feeReceipient;
 
@@ -215,6 +231,14 @@ contract SynthetixLimitOrders is Initializable, AuthUpgradable, ReentrancyGuardU
     }
 
     /// -----------------------------------------------------------------------
+    /// Views
+    /// -----------------------------------------------------------------------
+
+    function getFullOrder(uint256 id) external view returns (FullOrder memory order) {
+        order = fullOrders[id];
+    }
+
+    /// -----------------------------------------------------------------------
     /// Keeper Actions
     /// -----------------------------------------------------------------------
 
@@ -224,10 +248,8 @@ contract SynthetixLimitOrders is Initializable, AuthUpgradable, ReentrancyGuardU
         require(!order.isStarted, "order-executed");
         require(!order.isCancelled, "order-cancelled");
 
-        (uint256 currentPrice, bool invalid) = IPerpMarket(order.market).assetPrice();
-        require(!invalid && currentPrice != 0, "invalid-current-price");
-
-        require(order.priceA >= currentPrice && order.priceB <= currentPrice, "not-in-range");
+        (bool isInRange, uint256 currentPrice,) = isInPriceRange(order, true);
+        require(isInRange, "price-not-in-range");
 
         uint256 dollarValue = _abs(order.sizeDelta).mulWadDown(currentPrice);
         uint256 totalFees = flatFee + dollarValue.mulWadDown(percentageFee);
@@ -235,12 +257,12 @@ contract SynthetixLimitOrders is Initializable, AuthUpgradable, ReentrancyGuardU
         string[] memory targets = new string[](3);
         bytes[] memory datas = new bytes[](3);
 
-        targets[0] = "Synthetix-Perp-v1";
+        targets[0] = "Synthetix-Perp-v1.3";
         datas[0] = abi.encodeWithSignature(
             "trade(address,int256,uint256)", order.market, order.sizeDelta, order.priceImpactDelta
         );
 
-        targets[1] = "Synthetix-Perp-v1";
+        targets[1] = "Synthetix-Perp-v1.3";
         datas[1] =
             abi.encodeWithSignature("removeMargin(address,uint256,uint256,uint256)", order.market, totalFees, 0, 0);
 
@@ -268,13 +290,8 @@ contract SynthetixLimitOrders is Initializable, AuthUpgradable, ReentrancyGuardU
         require(!order.isCompleted, "pair-order-already-executed");
         require(!order.isCancelled, "order-cancelled");
 
-        (uint256 currentPrice, bool invalid) = IPerpMarket(order.market).assetPrice();
-        require(!invalid && currentPrice != 0, "invalid-current-price");
-
-        bool isInFirstRange = currentPrice >= order.firstPairA && currentPrice <= order.firstPairB;
-        bool isInSecondRange = currentPrice >= order.secondPairA && currentPrice <= order.secondPairB;
-
-        require(isInFirstRange || isInSecondRange, "not-in-range");
+        (bool isInRange, uint256 currentPrice, bool isInFirstRange) = isInPriceRange(order, true);
+        require(isInRange, "price-not-in-range");
 
         IPerpMarket.Position memory position = IPerpMarket(order.market).positions(order.user);
 
@@ -282,77 +299,95 @@ contract SynthetixLimitOrders is Initializable, AuthUpgradable, ReentrancyGuardU
             order.sizeDelta = position.size;
         }
 
-        uint256 dollarValue = _abs(order.sizeDelta).mulWadDown(currentPrice);
-        uint256 totalFees = flatFee + dollarValue.mulWadDown(percentageFee);
-
         if (order.firstPairA > 0 && order.firstPairB > 0 && isInFirstRange) {
-            string[] memory targets = new string[](3);
-            bytes[] memory datas = new bytes[](3);
-
-            targets[0] = "Synthetix-Perp-v1";
-            datas[0] = abi.encodeWithSignature(
-                "trade(address,int256,uint256)", order.market, -order.sizeDelta, order.priceImpactDelta
-            );
-
-            targets[1] = "Synthetix-Perp-v1";
-            datas[1] =
-                abi.encodeWithSignature("removeMargin(address,uint256,uint256,uint256)", order.market, totalFees, 0, 0);
-
-            targets[2] = "Basic-v1";
-            datas[2] = abi.encodeWithSignature(
-                "withdraw(address,uint256,address,uint256,uint256)",
-                0x8c6f28f2F1A3C87F0f938b96d27520d9751ec8d9,
-                totalFees,
-                feeReceipient,
-                0,
-                0
-            );
-
-            IAccount(order.user).cast(targets, datas, address(0x0));
-
-            order.isCompleted = true;
-
-            emit ExecutePairOrder(order.market, order.user, orderId, currentPrice, totalFees);
+            _executePairOrder(order, orderId, currentPrice);
 
             return;
         }
 
-        if (order.secondPairA > 0 && order.secondPairB > 0 && isInSecondRange) {
-            string[] memory targets = new string[](3);
-            bytes[] memory datas = new bytes[](3);
-
-            targets[0] = "Synthetix-Perp-v1";
-            datas[0] = abi.encodeWithSignature(
-                "trade(address,int256,uint256)", order.market, -order.sizeDelta, order.priceImpactDelta
-            );
-
-            targets[1] = "Synthetix-Perp-v1";
-            datas[1] =
-                abi.encodeWithSignature("removeMargin(address,uint256,uint256,uint256)", order.market, totalFees, 0, 0);
-
-            targets[2] = "Basic-v1";
-            datas[2] = abi.encodeWithSignature(
-                "withdraw(address,uint256,address,uint256,uint256)",
-                0x8c6f28f2F1A3C87F0f938b96d27520d9751ec8d9,
-                totalFees,
-                feeReceipient,
-                0,
-                0
-            );
-
-            IAccount(order.user).cast(targets, datas, address(0x0));
-
-            order.isCompleted = true;
-
-            emit ExecutePairOrder(order.market, order.user, orderId, currentPrice, totalFees);
+        if (order.secondPairA > 0 && order.secondPairB > 0 && !isInFirstRange) {
+            _executePairOrder(order, orderId, currentPrice);
 
             return;
         }
     }
 
+    function _executePairOrder(FullOrder memory order, uint256 orderId, uint256 currentPrice) internal {
+        uint256 dollarValue = _abs(order.sizeDelta).mulWadDown(currentPrice);
+        uint256 totalFees = flatFee + dollarValue.mulWadDown(percentageFee);
+
+        string[] memory targets = new string[](3);
+        bytes[] memory datas = new bytes[](3);
+
+        targets[0] = "Synthetix-Perp-v1.3";
+        datas[0] = abi.encodeWithSignature(
+            "trade(address,int256,uint256)", order.market, -order.sizeDelta, order.priceImpactDelta
+        );
+
+        targets[1] = "Synthetix-Perp-v1.3";
+        datas[1] =
+            abi.encodeWithSignature("removeMargin(address,uint256,uint256,uint256)", order.market, totalFees, 0, 0);
+
+        targets[2] = "Basic-v1";
+        datas[2] = abi.encodeWithSignature(
+            "withdraw(address,uint256,address,uint256,uint256)",
+            0x8c6f28f2F1A3C87F0f938b96d27520d9751ec8d9,
+            totalFees,
+            feeReceipient,
+            0,
+            0
+        );
+
+        IAccount(order.user).cast(targets, datas, address(0x0));
+
+        order.isCompleted = true;
+
+        emit ExecutePairOrder(order.market, order.user, orderId, currentPrice, totalFees);
+    }
+
     /// -----------------------------------------------------------------------
     /// Internals
     /// -----------------------------------------------------------------------
+
+    /// @notice Returns whether an order can be executed or not based on current price
+    /// @param order Order struct
+    /// @param isFirst Whether the request is for the first order or pair order
+    function isInPriceRange(FullOrder memory order, bool isFirst) internal view returns (bool, uint256, bool) {
+        (uint256 currentPrice, bool invalid) = IPerpMarket(order.market).assetPrice();
+        if (currentPrice == 0 || invalid) return (false, currentPrice, false);
+
+        bytes32 pythId = pythIds[order.market];
+        IPyth.Price memory pythPrice = pyth.getPriceUnsafe(pythId);
+        uint256 actualPythPrice = _getWadPrice(pythPrice.price, pythPrice.expo);
+        uint256 priceDelta = actualPythPrice > currentPrice
+            ? (actualPythPrice - currentPrice).divWadDown(currentPrice)
+            : (currentPrice - actualPythPrice).divWadDown(actualPythPrice);
+
+        bool isPythPriceStale = pythPrice.price <= 0 || block.timestamp - pythPrice.publishTime > pythPriceTimeCutoff
+            || priceDelta > pythPriceDeltaCutoff;
+
+        if (isFirst) {
+            if (order.priceA >= currentPrice && order.priceB <= currentPrice) return (true, currentPrice, false);
+
+            if (!isPythPriceStale && order.priceA >= actualPythPrice && order.priceB <= actualPythPrice) {
+                return (true, actualPythPrice, false);
+            }
+        } else {
+            bool isInFirstRange = currentPrice >= order.firstPairA && currentPrice <= order.firstPairB;
+            bool isInSecondRange = currentPrice >= order.secondPairA && currentPrice <= order.secondPairB;
+
+            if (isInFirstRange || isInSecondRange) return (true, currentPrice, isInFirstRange);
+
+            isInFirstRange = actualPythPrice >= order.firstPairA && actualPythPrice <= order.firstPairB;
+            isInSecondRange = actualPythPrice >= order.secondPairA && actualPythPrice <= order.secondPairB;
+
+            if (!isPythPriceStale && (isInFirstRange || isInSecondRange)) {
+                return (true, actualPythPrice, isInFirstRange);
+            }
+        }
+
+        return (false, currentPrice, false);
+    }
 
     /// @notice Returns whether an address is a SCW or not
     function isAllowed() internal view returns (bool) {
@@ -365,6 +400,19 @@ contract SynthetixLimitOrders is Initializable, AuthUpgradable, ReentrancyGuardU
 
     function _abs(int256 x) internal pure returns (uint256) {
         return uint256(_signedAbs(x));
+    }
+
+    function _getWadPrice(int64 price, int32 expo) internal pure returns (uint256 wadPrice) {
+        uint256 exponent = _abs(expo);
+        uint256 lastPrice = _abs(price);
+
+        if (exponent >= 18) {
+            uint256 denom = 10 ** (exponent - 18);
+            wadPrice = lastPrice / denom;
+        } else {
+            uint256 multiplier = 10 ** (18 - exponent);
+            wadPrice = lastPrice * multiplier;
+        }
     }
 
     /// -----------------------------------------------------------------------
@@ -387,6 +435,29 @@ contract SynthetixLimitOrders is Initializable, AuthUpgradable, ReentrancyGuardU
         percentageFee = _percentageFee;
     }
 
+    /// @notice Update Pyth Oracle ID
+    /// @param market Address of the market
+    /// @param id New Pyth Oracle ID
+    function updatePythOracleId(address market, bytes32 id) external requiresAuth {
+        emit UpdatePythId(market, pythIds[market], id);
+
+        pythIds[market] = id;
+    }
+
+    /// @notice Update Pyth Time Cutoff
+    /// @param newCutoff New cutoff
+    function updatePythTimeCutoff(uint256 newCutoff) external requiresAuth {
+        emit UpdatePythTimeCutoff(pythPriceTimeCutoff, newCutoff);
+
+        pythPriceTimeCutoff = newCutoff;
+    }
+
+    function updatePythDeltaCutoff(uint256 newCutoff) external requiresAuth {
+        emit UpdatePythDeltaCutoff(pythPriceDeltaCutoff, newCutoff);
+
+        pythPriceDeltaCutoff = newCutoff;
+    }
+
     /// -----------------------------------------------------------------------
     /// Events
     /// -----------------------------------------------------------------------
@@ -402,6 +473,22 @@ contract SynthetixLimitOrders is Initializable, AuthUpgradable, ReentrancyGuardU
     /// @param oldPercentage Old percentage fee rate
     /// @param newPercentage New percentage fee rate
     event UpdateFees(uint256 oldFlat, uint256 newFlat, uint256 oldPercentage, uint256 newPercentage);
+
+    /// @notice Emitted when Pyth oracle IDs are updated
+    /// @param market Address of the market
+    /// @param oldId Old Pyth Oracle ID
+    /// @param newId New Pyth Oracle ID
+    event UpdatePythId(address indexed market, bytes32 oldId, bytes32 newId);
+
+    /// @notice Emitted when Pyth price time cutoff is updated
+    /// @param oldCutoff Old cutoff
+    /// @param newCutoff New cutoff
+    event UpdatePythTimeCutoff(uint256 oldCutoff, uint256 newCutoff);
+
+    /// @notice Emitted when Pyth price delta cutoff is updated
+    /// @param oldCutoff Old Cutoff
+    /// @param newCutoff New Cutoff
+    event UpdatePythDeltaCutoff(uint256 oldCutoff, uint256 newCutoff);
 
     /// @notice Emitted when an order is submitted
     /// @param market Address of the perp market
