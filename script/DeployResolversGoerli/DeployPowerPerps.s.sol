9e6d18de81b4579b8b7399719cd4e5ef462c0a8e
ankitpal1029 <=>
2023-07-05 15:36:44 +0530

    👺 redeploy resolver

--- script/DeployResolversGoerli/DeployPowerPerps.s.sol
+++ script/DeployResolversGoerli/DeployPowerPerps.s.sol
@@ -10,7 +10,7 @@ contract DeployPowerPerpResolver is Script {
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
-        new PowerPerpResolver(0xf80DC069cD185467495BfA9baB6A25f1eb810e1D);
+        new PowerPerpResolver(0x5313f042D3cB1C6428bD4Ab987d63502e70A314f);
         vm.stopBroadcast();
     }
 }
