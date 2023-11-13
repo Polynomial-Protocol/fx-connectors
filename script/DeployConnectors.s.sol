284c8f885d5c4c676c406b0905a5ed95ab97d5b1
Mubaris NK <hello@mubaris.com>
2023-11-13 12:05:51 +0300

    🥶 redploy perp v3

--- script/DeployConnectors.s.sol
+++ script/DeployConnectors.s.sol
@@ -32,7 +32,7 @@ contract DeployConnectors is Script {
         address[] memory addrs = new address[](1);
 
         SynthetixPerpV3Connector snxV3 =
-        new SynthetixPerpV3Connector(0x9863Dae3f4b5F4Ffe3A841a21565d57F2BA10E87, 0x17633A63083dbd4941891F87Bdf31B896e91e2B9, 0x579c612E4Bf390f5504DB9f76b6F5759A3172279, 0x9849832a1d8274aaeDb1112ad9686413461e7101);
+        new SynthetixPerpV3Connector(0xEED61f0CB02f3B38923b1b6EAa939D5f04f431b6, 0x41A883a85b1AdE59F41d459Fa550b40fa56429DB, 0xC9ee9628f23b14483EA413C28712690E8D2dC6a3, 0x9849832a1d8274aaeDb1112ad9686413461e7101);
 
         names[0] = snxV3.name();
 
