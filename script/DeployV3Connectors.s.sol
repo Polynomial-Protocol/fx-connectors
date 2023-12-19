abd6d798a5444666eac47c2ffc53e6c542038941
Mubaris NK <hello@mubaris.com>
2023-12-19 13:46:18 +0400

    🐧 v3 connector with fee addr

--- script/DeployV3Connectors.s.sol
+++ script/DeployV3Connectors.s.sol
@@ -20,14 +20,14 @@ contract DeployConnectors is Script {
 
         IConnectors connectors = IConnectors(0x90c5D64a67425d03774439d8d37194B29C2070FA);
 
-        string[] memory names = new string[](2);
-        address[] memory addrs = new address[](2);
+        string[] memory names = new string[](1);
+        address[] memory addrs = new address[](1);
 
-        SynthetixSpotV3Connector snxV3 = new SynthetixSpotV3Connector(
-            0x18141523403e2595D31b22604AcB8Fc06a4CaA61,
-            0x09d51516F38980035153a554c26Df3C6f51a23C3,
-            0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913
-        );
+        // SynthetixSpotV3Connector snxV3 = new SynthetixSpotV3Connector(
+        //     0x18141523403e2595D31b22604AcB8Fc06a4CaA61,
+        //     0x09d51516F38980035153a554c26Df3C6f51a23C3,
+        //     0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913
+        // );
 
         SynthetixPerpV3Connector perpV3 = new SynthetixPerpV3Connector(
             0x0A2AF931eFFd34b81ebcc57E3d3c9B1E1dE1C9Ce,
@@ -36,13 +36,13 @@ contract DeployConnectors is Script {
             0xEb38e347F24ea04ffA945a475BdD949E0c383A0F
         );
 
-        names[0] = snxV3.name();
-        addrs[0] = address(snxV3);
+        // names[0] = snxV3.name();
+        // addrs[0] = address(snxV3);
 
-        names[1] = perpV3.name();
-        addrs[1] = address(perpV3);
+        names[0] = perpV3.name();
+        addrs[0] = address(perpV3);
 
-        connectors.addConnectors(names, addrs);
+        connectors.updateConnectors(names, addrs);
 
         vm.stopBroadcast();
     }
