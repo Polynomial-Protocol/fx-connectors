2b8c2f463f3a2cef92997509e2054ef42c4a8bda
Md Sabbir Rahman <33892003+upobir@users.noreply.github.com>
2024-02-23 13:32:08 +0600

    🧤 add scw = msg.sender check in limit order (#43)
    * add scw = msg.sender check in limit order
    
    * 🧑‍🔧 deploy
    
    ---------
    
    Co-authored-by: Mubaris NK <hello@mubaris.com>

--- script/base_sepolia/UpdateLimitOrderV3.s.sol
+++ script/base_sepolia/UpdateLimitOrderV3.s.sol
@@ -7,7 +7,7 @@ import {console2} from "forge-std/console2.sol";
 import {SynthetixLimitOrdersV3} from "../../src/common/automations/SynthetixLimitOrdersV3.sol";
 import {TransparentUpgradeableProxy} from "../../src/common/proxy/TransparentUpgradeProxy.sol";
 
-contract DeployLimitOrder is Script {
+contract UpdateLimitOrderV3 is Script {
     address payable constant LIMIT_ORDER_PROXY = payable(0xf20E2ed5cf876C8E25996D2dAB9DDe51D682DfE6);
 
     function run() external {
